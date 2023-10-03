138caeda30b33e5a4668e870ecdf8f1f669b6d46
Pavel Sh <moshushi@gmail.com>
2023-10-04 01:06:14 +0300

    Разобраться с pytest запуском из консоли

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,16 @@
-def summator(a, b):
-    return a + b
+import pytest
+
+from src.summator import summator
+
+
+# from ../src/summator import summator
+# from summator import summator
 
 def test_one():
     assert summator(10, 20) == 30
 
+
+@pytest.mark.smoke
 def test_two():
-    assert summator(-10, -7) == 20
+    #    assert summator(-10, -7) == 20
+    assert summator(-10, -7) == -17
